74586a91f1e4af92f93c7e28af881dc54a4f5cf8
Josh Buker <crypto@joshbuker.com>
2021-12-17 22:43:37 -0800

    Get JSON blob rendering

--- src/pages/Identifier.vue
+++ src/pages/Identifier.vue
@@ -1,13 +1,66 @@
 <template>
   <q-page class="flex flex-center">
-    <h1>{{ $route.params.id }}</h1>
+    <div class="row">
+      <div class="col-grow">
+        <div class="q-pa-md" style="max-width: 100vw;">
+          <q-expansion-item
+            class="shadow-1 overflow-hidden"
+            style="border-radius: 30px;"
+            icon="terminal"
+            label="JSON Data"
+            header-class="bg-primary text-white"
+            expand-icon-class="text-white"
+          >
+            <q-card style="background: #2d2d2d;">
+              <q-card-section style="overflow: auto;">
+                <pre class="line-numbers" v-if="jsonBlob"><code class="language-json">{{ jsonBlob }}</code></pre>
+                <p v-else style="color: white;">Invalid identifier.</p>
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref, watch, nextTick } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { api } from 'boot/axios'
+
+// Syntax highlighting for JSON
+import Prism from 'prismjs'
 
 export default defineComponent({
-  name: 'PageIdentifier'
+  name: 'PageIdentifier',
+
+  setup() {
+    const $route = useRoute()
+    const $router = useRouter()
+
+    const identifier = $route.params.id
+    const jsonBlob = ref('')
+
+    if (identifier.match(/^GSD-\d{4}-\d{7,}$/) || identifier.match(/^UVI-\d{4}-\d{7,}$/)) {
+      api.get(`/${identifier}`).then(
+        (response) => {
+          jsonBlob.value = response.data
+        },
+        (error) => {}
+      )
+    }
+
+    watch(
+      () => jsonBlob.value,
+      (newValue) => {
+        nextTick().then(
+          () => { Prism.highlightAll() }
+        )
+      }
+    )
+
+    return { jsonBlob }
+  }
 })
 </script>
