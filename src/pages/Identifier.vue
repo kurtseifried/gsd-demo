0d7b220220625c7a20331e273d2eddc3e03207ac
Josh Buker <crypto@joshbuker.com>
2021-12-20 20:28:42 -0800

    Hack together simple tables - still WIP

--- src/pages/Identifier.vue
+++ src/pages/Identifier.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="row">
+  <q-page>
+    <div class="row items-start justify-evenly q-col-gutter-md q-ma-md">
       <div class="col-grow">
         <div class="q-pa-md" style="max-width: 100vw;">
           <q-expansion-item
@@ -29,11 +29,129 @@
         </div>
       </div>
     </div>
+
+    <div class="row items-start justify-evenly q-col-gutter-md q-ma-md" v-if="cveId">
+      <div class="col-grow">
+        <q-card bordered class="q-pa-md">
+          <q-card-section>
+            <div class="text-h5">{{ cveId }} - GSD Overlay Data</div>
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-section horizontal>
+            <table class="fancy-table">
+              <tr>
+                <td>Description</td>
+                <td><div style="white-space: pre-line;">{{ overlayDescription }}</div></td>
+              </tr>
+              <tr>
+                <td>State</td>
+                <td>{{ cveState }}</td>
+              </tr>
+              <tr>
+                <td>Problem Types</td>
+                <td>
+                  <ul>
+                    <li v-for="(problem, index) in cveProblemTypes" :key="index">{{ problem }}</li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td>Vendors, Projects & Versions</td>
+                <td>
+                  <ul>
+                    <li><b>Vendor:</b>&nbsp;{{ cveVendor }}</li>
+                    <li><b>Product:</b>&nbsp;{{ cveProduct }}</li>
+                    <li><b>Versions Affected:</b>
+                      <ul>
+                        <li v-for="(version, index) in cveVersionsAffected" :key="index">
+                          <b>{{ version.version_name }}:</b> {{ version.version_affected }} {{ version.version_value }}
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td>References</td>
+                <td>
+                  <ul>
+                    <li v-for="(reference, index) in overlayReferences" :key="index">
+                      <b>{{ reference.refsource }}</b> - <a :href="reference.url" target="_blank">{{ reference.name }}</a>
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+            </table>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+
+    <div class="row items-start justify-evenly q-col-gutter-md q-ma-md" v-if="cveId">
+      <div class="col-grow">
+        <q-card bordered class="q-pa-md">
+          <q-card-section>
+            <div class="text-h5">{{ cveId }} - Original Data</div>
+          </q-card-section>
+
+          <q-separator />
+
+          <q-card-section horizontal>
+            <table class="fancy-table">
+              <tr>
+                <td>Description</td>
+                <td>{{ cveDescription }}</td>
+              </tr>
+              <tr>
+                <td>State</td>
+                <td>{{ cveState }}</td>
+              </tr>
+              <tr>
+                <td>Problem Types</td>
+                <td>
+                  <ul>
+                    <li v-for="(problem, index) in cveProblemTypes" :key="index">{{ problem }}</li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td>Vendors, Projects & Versions</td>
+                <td>
+                  <ul>
+                    <li><b>Vendor:</b>&nbsp;{{ cveVendor }}</li>
+                    <li><b>Product:</b>&nbsp;{{ cveProduct }}</li>
+                    <li><b>Versions Affected:</b>
+                      <ul>
+                        <li v-for="(version, index) in cveVersionsAffected" :key="index">
+                          <b>{{ version.version_name }}:</b> {{ version.version_affected }} {{ version.version_value }}
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <td>References</td>
+                <td>
+                  <ul>
+                    <li v-for="(reference, index) in cveReferences" :key="index">
+                      <b>{{ reference.refsource }}</b> - <a :href="reference.url" target="_blank">{{ reference.name }}</a>
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+            </table>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent, ref, watch, nextTick } from 'vue'
+import { defineComponent, ref, watch, nextTick, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { api } from 'boot/axios'
 
@@ -59,6 +177,124 @@ export default defineComponent({
       )
     }
 
+    // const gsdOverlayData
+    // const cveOriginalData
+
+    const overlayDescription = computed({
+      get: () => {
+        if(jsonBlob.value["gsd"]["overlay_data"]["cve.org"]) {
+          const desc = jsonBlob.value["gsd"]["overlay_data"]["cve.org"]["description"]["description_data"][0]["value"]
+          return desc ? desc : 'Error - Description unavailable.'
+        }
+
+        return ''
+      }
+    })
+
+    // TODO: Does not include original references. Should we interpolate? Or update gsd references to include originals? See GSD-2021-1002353 for example of issue.
+    const overlayReferences = computed({
+      get: () => {
+        if(jsonBlob.value["gsd"]["references"]) {
+          const desc = jsonBlob.value["gsd"]["references"]
+          return desc ? desc : 'Error - Description unavailable.'
+        }
+
+        return ''
+      }
+    })
+
+    const cveDescription = computed({
+      get: () => {
+        if(jsonBlob.value["cve.org"]) {
+          const desc = jsonBlob.value["cve.org"]?.description?.description_data[0]?.value
+          return desc ? desc : 'Error - Description unavailable.'
+        }
+
+        return ''
+      }
+    })
+
+    const cveId = computed({
+      get: () => {
+        if(jsonBlob.value["cve.org"]) {
+          const desc = jsonBlob.value["cve.org"]["CVE_data_meta"]["ID"];
+          return desc ? desc : 'Error - Description unavailable.'
+        }
+
+        return ''
+      }
+    })
+
+    const cveState = computed({
+      get: () => {
+        if(jsonBlob.value["cve.org"]) {
+          const desc = jsonBlob.value["cve.org"]["CVE_data_meta"]["STATE"];
+          return desc ? desc : 'Error - Description unavailable.'
+        }
+
+        return ''
+      }
+    })
+
+    const cveProblemTypes = computed({
+      get: () => {
+        if(jsonBlob.value["cve.org"]) {
+          const desc = jsonBlob.value["cve.org"]["problemtype"]["problemtype_data"].map(
+            (val) => {
+              return val.description[0].value
+            }
+          );
+          return desc ? desc : 'Error - Description unavailable.'
+        }
+
+        return ''
+      }
+    })
+
+    const cveVendor = computed({
+      get: () => {
+        if(jsonBlob.value["cve.org"]) {
+          const desc = jsonBlob.value["cve.org"]["affects"]["vendor"]["vendor_data"][0]["vendor_name"]
+          return desc ? desc : 'Error - Description unavailable.'
+        }
+
+        return ''
+      }
+    })
+
+    const cveProduct = computed({
+      get: () => {
+        if(jsonBlob.value["cve.org"]) {
+          const desc = jsonBlob.value["cve.org"]["affects"]["vendor"]["vendor_data"][0]["product"]["product_data"][0]["product_name"]
+          return desc ? desc : 'Error - Description unavailable.'
+        }
+
+        return ''
+      }
+    })
+
+    const cveVersionsAffected = computed({
+      get: () => {
+        if(jsonBlob.value["cve.org"]) {
+          const desc = jsonBlob.value["cve.org"]["affects"]["vendor"]["vendor_data"][0]["product"]["product_data"][0]["version"]["version_data"]
+          return desc ? desc : 'Error - Description unavailable.'
+        }
+
+        return ''
+      }
+    })
+
+    const cveReferences = computed({
+      get: () => {
+        if(jsonBlob.value["cve.org"]) {
+          const desc = jsonBlob.value["cve.org"]["references"]["reference_data"]
+          return desc ? desc : 'Error - Description unavailable.'
+        }
+
+        return ''
+      }
+    })
+
     watch(
       () => jsonBlob.value,
       (newValue) => {
@@ -68,7 +304,39 @@ export default defineComponent({
       }
     )
 
-    return { jsonBlob, identifier }
+    return {
+      jsonBlob,
+      identifier,
+      cveId,
+      cveDescription,
+      cveState,
+      cveProblemTypes,
+      cveVendor,
+      cveProduct,
+      cveVersionsAffected,
+      cveReferences,
+      overlayDescription,
+      overlayReferences,
+    }
   }
 })
 </script>
+
+<style lang="sass">
+// FIXME: I hate css, and it hates me. Please make this look prettier.
+.fancy-table
+  width: 100%
+  border: 1px solid
+  border-collapse: collapse
+  td
+    border: 1px solid #ddd
+    padding: 8px
+  tr:nth-child(even)
+    background-color: #f2f2f2
+  td:hover // I'm very tempted to remove this hover effect, tbh.
+    background-color: #ddd
+  td:nth-child(odd)
+    background-color: #0b2f5e
+    color: white
+    font-weight: bold
+</style>
