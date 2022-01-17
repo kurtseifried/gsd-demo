8ec8009f644ad87189a0eb2889ee524636c2fb44
Josh Buker <crypto@joshbuker.com>
2022-01-17 14:02:15 -0800

    Update home page logo to CSA logo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+      alt="Cloud Security Alliance logo"
+      src="~assets/csa-logo.svg"
+      style="width: 75vw; max-width: 600px;"
     >
   </q-page>
 </template>
